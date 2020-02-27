55823e24a2da9a6e1b0b637f70d82511e279672d
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-02-27 16:09:53 +0100

    Actualizado con login y signin Ahora el formulario de login y signup se genera automatico. Ya validamos los campos

--- form_with_boostrap/flask_bootstrap/app.py
+++ form_with_boostrap/flask_bootstrap/app.py
@@ -1,22 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length, Email
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'EstoDeflask_wtfEsLaPolla!'
 Bootstrap(app)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('Remember me')
+
+
+class SignupForm(FlaskForm):
+    username = StringField('User Name', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    email = StringField('Email address', validators=[InputRequired(), Email(message='Invalid email'), Length(max=15)])
+
 
 @app.route('/')
 def index():
     return render_template("index.html", page="index")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", page="login")
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if form.password.data == '12345678':
+                return redirect(url_for('dashboard'))
+            else:
+                return "Access denied"
+    else:
+        pass
+    return render_template("login.html", page="login", form=form)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html", page="signup")
+    form = SignupForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            return generate_password_hash(form.password.data, method='sha256')
+#            return redirect(url_for('login'))
+        else:
+            pass
+    else:
+        pass
+    return render_template("signup.html", page="signup", form=form)
 
 
 @app.route('/dashboard')
